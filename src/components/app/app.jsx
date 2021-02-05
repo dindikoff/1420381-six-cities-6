00bb6c0fd666bb1f3abe93be462c46aa1878fe45
Stanislau <savandex@gmail.com>
2021-02-05 09:48:39 -0800

    Добавляет маршрутизацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import MainPage from '../main-page/main-page';
+import FavoritesPage from '../favorites-page/favorites-page';
+import LoginPage from '../login-page/login-page';
+import RoomPage from '../room-page/room-page';
+import Page404 from '../page404/Page404';
+
 import {CardType} from '../card/card';
+
 
 const App = (props) => {
   const {cards} = props;
 
   return (
-    <MainPage cards={cards} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage cards={cards} />
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesPage />
+        </Route>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+        <Route exact path="/offer/:id">
+          <RoomPage />
+        </Route>
+        <Route>
+          <Page404 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 const RATING_STAR_PERCENT = 20;
 
@@ -22,7 +23,7 @@ const Card = ({card}) => {
     name, image,
     price, type,
     isPremium, rating,
-    isFavorite,
+    isFavorite, id
   } = card;
 
   return (
@@ -52,19 +53,19 @@ const Card = ({card}) => {
             `place-card__bookmark-button button ${isFavorite ? `place-card__bookmark-button--active` : ``}`
           } type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
-              <use xlinkHref="#icon-bookmark"></use>
+              <use xlinkHref="#icon-bookmark"/>
             </svg>
             <span className="visually-hidden">{isFavorite ? `In bookmarks` : `To bookmarks`}</span>
           </button>
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: rating * RATING_STAR_PERCENT}}></span>
+            <span style={{width: rating * RATING_STAR_PERCENT}}/>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">{name}</a>
+          <Link to={`offer/` + id}>{name}</Link>
         </h2>
         <p className="place-card__type">{typeToText[type]}</p>
       </div>
